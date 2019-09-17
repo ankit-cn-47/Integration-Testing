232e697ca2a653dc9d9a131c6806e1da4671572e
ankit-cn-47 <ankit.codenamefourty7@gmail.com>
2019-09-17 12:37:01 +0530

    Rocked the world

--- integration-testing-demo/src/app/app.module.ts
+++ integration-testing-demo/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TodosComponent } from './todos/todos.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { VoterComponent } from './voter/voter.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TodosComponent,
+    UserDetailsComponent,
+    VoterComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
